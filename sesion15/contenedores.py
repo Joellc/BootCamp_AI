6d47d58ce03a4cd46cef7515dc2091ca3820db25
Joellc <joellc2311@gmail.com>
2025-02-08 12:47:48 -0500

    merge de local y master

---
+++ sesion15/contenedores.py
@@ -0,0 +1,23 @@
+#--------------------Pagina 3--------------------
+#Contenedores
+#Listas
+#Las listas van en corchetes [  ]
+
+pokemones_ash = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Pidgey"]
+print(pokemones_ash)
+print(pokemones_ash[0])
+#Indice y acceso a elementos: los elementos de una lista se enumeran del 0 e adelante
+primer_pokemon = pokemones_ash[0]
+print(primer_pokemon)
+#Crea una variable llamasa segundo_pokemon y muestralo en pantalla
+segundo_pokemon = pokemones_ash[1]
+print(segundo_pokemon)
+
+#------------------Pagina 4--------------------
+#Longitud de una lista: puedes obtener la longitud de una lista con la función len()
+print(len(pokemones_ash))
+print(pokemones_ash)
+pokemones_ash[3] = "Tauros"
+print(pokemones_ash)
+#------------------Pagina 5--------------------
+#Añadir elementos a una lista
